bf2e85218c05f71f22c6b35599752dd73ec28f98
Joe Pym <hitonagashi@gmail.com>
2012-05-15 00:25:16 +0100

    Added selected and hover states for the team slider bar

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -4,12 +4,14 @@ class TeamsController < ApplicationController
     @team = Team.find(params[:id])
     @players = @team.players
     @roster = @team.roster
+    @rosters = Roster.all.sort{|x, y| x.name <=> y.name}
   end
 
   def index
     @team = Team.find(3)
     @players = @team.players
     @roster = @team.roster
+    @rosters = Roster.all.sort{|x, y| x.name <=> y.name}
     render "show"
   end
 end
